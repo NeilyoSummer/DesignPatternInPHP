11fcdef9fa264c9f7ef2a20b6c8f3b0b8d3e37a0
neilyosummer <neilyosummer@163.com>
2019-11-02 22:48:22 +0800

    调整结构

--- src/Behavioral/Strategy/FullReductionStrategy.php
+++ src/Behavioral/Strategy/FullReductionStrategy.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DesignPattern\Strategy;
+namespace DesignPattern\Behavioral\Strategy;
 
 class FullReduction extends StrategyAbstract
 {
--- src/Behavioral/Strategy/StrategyContext.php
+++ src/Behavioral/Strategy/StrategyContext.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DesignPattern\Strategy;
+namespace DesignPattern\Behavioral\Strategy;
 
 class StrategyContext
 {
